e02454d75c0aa9ced32e214bb289367a3aa81b3c
Jaime Pineda <jpin730@pm.me>
2023-06-13 17:09:41 -0600

    style: :lipstick: add calendar event component

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,15 +1,16 @@
 export const Navbar = () => {
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
-      <span className="navbar-brand">
-        <i className="fas fa-calendar-alt me-2"></i>
-        MERN Calendar
-      </span>
-
-      <button className="btn btn-outline-danger">
-        <i className="fas fa-sign-out-alt me-2"></i>
-        <span>Logout</span>
-      </button>
+      <div className="container">
+        <span className="navbar-brand">
+          <i className="fas fa-calendar-alt me-2"></i>
+          MERN Calendar
+        </span>
+        <button className="btn btn-outline-danger">
+          <i className="fas fa-sign-out-alt me-2"></i>
+          <span>Logout</span>
+        </button>
+      </div>
     </div>
   );
 };
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,43 +1,72 @@
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { Calendar } from "react-big-calendar";
+import { addHours } from "date-fns";
+import { useState } from "react";
+
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { format, parse, startOfWeek, getDay, addHours } from "date-fns";
-import enUS from "date-fns/locale/en-US";
 
-import { Navbar } from "../components/Navbar";
-
-const locales = {
-  "en-US": enUS,
-};
+import { CalendarEvent, Navbar } from "../components";
+import { localizer } from "../../helpers";
 
 const events = [
   {
     title: "Boss' Birthday",
-    notes: "Buy a cake",
     start: new Date(),
     end: addHours(new Date(), 2),
   },
 ];
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
-
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "week"
+  );
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#347cf7",
+      borderRadius: 0,
+      opacity: 0.8,
+      color: "white",
+    };
+
+    return { style };
+  };
+
+  const onDoubleClick = (event) => {
+    console.log({ doubleClick: event });
+    // openDateModal();
+  };
+
+  const onSelect = (event) => {
+    console.log({ click: event });
+    // setActiveEvent( event );
+  };
+
+  const onViewChanged = (event) => {
+    localStorage.setItem("lastView", event);
+    setLastView(event);
+  };
+
   return (
     <>
       <Navbar />
 
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: "calc(100vh - 80px)" }}
-      />
+      <div className="container">
+        <Calendar
+          startAccessor="start"
+          endAccessor="end"
+          defaultView={lastView}
+          localizer={localizer}
+          events={events}
+          style={{ height: "calc(100vh - 100px)" }}
+          eventPropGetter={eventStyleGetter}
+          components={{
+            event: CalendarEvent,
+          }}
+          onDoubleClickEvent={onDoubleClick}
+          onSelectEvent={onSelect}
+          onView={onViewChanged}
+        />
+      </div>
     </>
   );
 };
