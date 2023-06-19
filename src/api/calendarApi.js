e8d3da62950a74012240044aaf8e302887c39e00
Jaime Pineda <jpin730@pm.me>
2023-06-18 20:21:08 -0600

    feat: :passport_control: add token header interceptor

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -1,11 +1,25 @@
 import axios from "axios";
 
+import { AUTH_LOCAL_STORAGE } from "../auth";
 import { getEnv } from "../helpers";
 
 const { VITE_API_URL } = getEnv();
 
 const calendarApi = axios.create({
   baseURL: VITE_API_URL,
+});
+
+calendarApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem(AUTH_LOCAL_STORAGE.token);
+
+  if (token) {
+    config.headers = {
+      ...config.headers,
+      "x-token": token,
+    };
+  }
+
+  return config;
 });
 
 export default calendarApi;
